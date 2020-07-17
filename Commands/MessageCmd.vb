cc4967b4c633a6e235ee73ff1e2631a2cec93c65
black death <68387396+Windows-1252@users.noreply.github.com>
2020-07-17 17:45:28 +0200

    Add MessageCmd and StartCmd

---
+++ Commands/MessageCmd.vb
@@ -0,0 +1,8 @@
+﻿Public Class MessageCmd
+    Implements ICommand
+    Public ReadOnly Property Name As String = "msg" Implements ICommand.Name
+
+    Public Sub Execute(args As String, sender As String, guac As Guacamole) Implements ICommand.Execute
+        MessageBox.Show(args, $"{sender} says...", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+    End Sub
+End Class
---
+++ Commands/StartCmd.vb
@@ -0,0 +1,21 @@
+﻿Public Class StartCmd
+    Implements ICommand
+    Public ReadOnly Property Name As String = "start" Implements ICommand.Name
+
+    Public Sub Execute(args As String, sender As String, guac As Guacamole) Implements ICommand.Execute
+        Dim splitArgs = args.Split(New Char() {","c}, 2)
+
+        If splitArgs.Length > 0 Then
+            Dim exec = splitArgs(0).Trim()
+            Dim execArgs = If(splitArgs.Length = 2, splitArgs(1).Trim(), "")
+
+            Try
+                Process.Start(exec, execArgs)
+                guac.Chat($"@{sender} {exec} started.")
+            Catch ex As Exception
+                guac.Chat($"@{sender} Cannot start {exec}.")
+            End Try
+
+        End If
+    End Sub
+End Class
